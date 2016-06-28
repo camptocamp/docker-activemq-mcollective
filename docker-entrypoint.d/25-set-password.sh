b6edaee5a8f3c3e93a50d36952bfb2f6e91ab699
Diego Abelenda <diego.abelenda@camptocamp.com>
2016-06-28 09:50:45 +0200

    Stop installing activemq from packages use the official release from apache

--- docker-entrypoint.d/25-set-password.sh
+++ docker-entrypoint.d/25-set-password.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-/opt/puppetlabs/puppet/bin/augtool -Ast "Xml incl /etc/activemq/instances-available/main/activemq.xml" "set //authenticationUser[#attribute/username = 'mcollective']/#attribute/password $STOMP_PASSWORD"
-/opt/puppetlabs/puppet/bin/augtool -Ast "Xml incl /etc/activemq/instances-available/main/activemq.xml" "set //authenticationUser[#attribute/username = 'admin']/#attribute/password $(pwgen -s 12 1)"
+/opt/puppetlabs/puppet/bin/augtool -Ast "Xml incl /opt/activemq/conf/activemq.xml" "set //authenticationUser[#attribute/username = 'mcollective']/#attribute/password $STOMP_PASSWORD"
+/opt/puppetlabs/puppet/bin/augtool -Ast "Xml incl /opt/activemq/conf/activemq.xml" "set //authenticationUser[#attribute/username = 'admin']/#attribute/password $(pwgen -s 12 1)"
