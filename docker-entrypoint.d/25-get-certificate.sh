b58b9477109d33620a03b025107b741a73335e91
Mickaël Canévet <mickael.canevet@camptocamp.com>
2016-12-07 13:55:22 +0100

    Allow to create a certificate with DNS Alt Names

--- docker-entrypoint.d/25-get-certificate.sh
+++ docker-entrypoint.d/25-get-certificate.sh
@@ -12,6 +12,11 @@ custom_attributes:
   1.2.840.113549.1.9.7: '$(curl http://rancher-metadata/latest/self/service/name 2> /dev/null):$(curl http://rancher-metadata/latest/self/service/uuid 2> /dev/null)'
 EOF
 
+  # Set DNSAltNames
+  if test -n "${DNS_ALT_NAMES}"; then
+    puppet config set dns_alt_names $DNS_ALT_NAMES --section main
+  fi
+
   # Get certificate
   rc=1
   while test $rc -ne 0; do
