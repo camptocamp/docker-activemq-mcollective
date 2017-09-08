5c158f3b397f7089c51a566196a582b28101e7bf
Mickaël Canévet <mickael.canevet@camptocamp.com>
2017-09-08 14:21:55 +0200

    Allow autosign from psk

--- docker-entrypoint.d/25-get-certificate.sh
+++ docker-entrypoint.d/25-get-certificate.sh
@@ -27,4 +27,33 @@ EOF
     fi
     rc=$?
   done
+elif test -n "${AUTOSIGN_PSK}"; then
+  PATH=/opt/puppetlabs/bin:$PATH
+  confdir=$(puppet config print confdir)
+
+  # Generate csr_attributes.yaml
+  cat << EOF > ${confdir}/csr_attributes.yaml
+---
+custom_attributes:
+  1.2.840.113549.1.9.7: 'hashed;$(CERTNAME=$(puppet config print certname) ruby -e 'require "openssl"; print Digest::SHA256.base64digest(ENV["AUTOSIGN_PSK"] + "/" + ENV["CERTNAME"] + "/puppetdb/production")')'
+extension_requests:
+  pp_role: puppetdb
+  pp_environment: production
+EOF
+
+  # Set DNSAltNames
+  if test -n "${DNS_ALT_NAMES}"; then
+    puppet config set dns_alt_names $DNS_ALT_NAMES --section main
+  fi
+
+  # Get certificate
+  rc=1
+  while test $rc -ne 0; do
+    if getent hosts puppetca > /dev/null; then
+      puppet agent -t --noop --server puppetca
+    else
+      puppet agent -t --noop
+    fi
+    rc=$?
+  done
 fi
